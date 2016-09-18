d9b4f850ae5a35dd8c28d10dc75b965ff14b44ab
Erik <zlinghan@hawk.iit.edu>
2016-09-17 23:10:59 -0700

    local mongo and twitter streaming

---
+++ mongo/m.py
@@ -0,0 +1,73 @@
+# coding=utf-8
+
+from pymongo import MongoClient
+
+__author__ = "linghanzhao"
+__created__ = "9/17/16"
+
+"""
+File Description
+"""
+
+MONGOCLIENT = None
+
+
+def get_client():
+    """
+    This function is to get the Mongo client
+    :param host:
+    :param user:
+    :param password:
+    :return:
+    """
+
+    global MONGOCLIENT
+
+    if MONGOCLIENT is not None:
+        return MONGOCLIENT
+
+    MONGOCLIENT = MongoClient(host="localhost")
+    return MONGOCLIENT
+
+
+def get_db(name='twitters'):  # twitters database
+    """
+    This function returns the database for the current application
+    :param name:
+    :return:
+    """
+    try:
+        client = get_client()
+        db = client.get_database(name)
+    except Exception as e:
+        print e
+        return None
+    return db
+
+
+def get_collections(db=None):
+    """This function returns all collections in database.
+
+    :param db: database.
+    :type db: pymongo.database.Database.
+    :return collections: all collections in database.
+    :rtype collections: dict
+
+    """
+
+    if db is None:
+        return None
+
+    # Collections name
+    twitter_streaming = db.twitter_streaming
+
+    # collections (and databases) in MongoDB is that they are created lazily,
+    # - none of the above commands have actually performed any operations on the MongoDB server.
+    # Collections and databases are created when the first document is inserted into them.
+
+    collections = {
+        'twitter_streaming': twitter_streaming,
+    }
+    return collections
+
+
---
+++ twitter/twitter_operation.py
@@ -0,0 +1,65 @@
+# coding=utf-8
+
+import tweepy
+from mongo import m
+
+__author__ = "linghanzhao"
+__created__ = "9/17/16"
+
+"""
+File Description
+"""
+
+consumer_key = 'WkUVCAmfBUdEUdf5R5fCF9Bi3'
+consumer_token = 'Bc7waqqWAkVK9WLkXg3ghqU3hb6T4k0nyg4fSZtj2MnQxsMfic'
+
+access_token = '753382617777512448-A0JXSUr0UCbm6gJB97BoPNsdvB5aWAJ'
+access_token_secret = 'J0K2dOJPXIbbh39knKQ7W0XLX2HU2clmsPB9dBeJmQoOq'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_token)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+stream_tweets_limit = 10
+
+db = m.get_db()
+collections = m.get_collections(db)
+
+
+# override tweepy.StreamListener to add logic to on_status and error
+class MyStreamListener(tweepy.StreamListener):
+    def __init__(self, api=None):
+        super(MyStreamListener, self).__init__()
+        self.num_tweets = 0
+        self.ids = []
+
+    def on_status(self, status):
+        if status.coordinates:
+            if self.num_tweets < stream_tweets_limit:
+                id = collections['twitter_streaming'].insert_one(
+                        {"text": status.text, "coordinates": status.coordinates['coordinates']})
+                self.ids.append(str(id))
+                self.num_tweets += 1
+                return True
+            else:
+                return False
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            # returning False in on_data disconnects the stream
+            return False
+
+
+def twitter_consumer():
+    """
+
+    :return:
+    """
+    myStreamListener = MyStreamListener()
+    myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+
+    myStream.filter(locations=[-180, -90, 180, 90], languages=['en'])
+    return myStreamListener.ids
+
+
+print twitter_consumer()
