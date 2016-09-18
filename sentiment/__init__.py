4564268ad3e7982b8c1b11671889a6cb22d1bad6
Erik <zlinghan@hawk.iit.edu>
2016-09-18 00:36:21 -0700

    2.0 sentiment function

---
+++ sentiment/__init__.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+
+
+__author__ = "linghanzhao"
+__created__ = "9/17/16"
+
+
+"""
+File Description
+"""
+
---
+++ sentiment/sentiment_analyze.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+
+import textblob
+
+__author__ = "linghanzhao"
+__created__ = "9/17/16"
+
+"""
+File Description
+"""
+
+
+def analyze_sentiment(text=None):
+    """
+    This function use textblob to analyze sentiment from text
+    :param text:
+    :return:
+    """
